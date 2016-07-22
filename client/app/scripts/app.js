e4bf43f89662b764470a61b9f9156142f92426d0
358203708 <358203708@qq.com>
2016-07-22 23:46:12 +0100

    change route.js

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -7,21 +7,34 @@
  *
  * Main module of the application.
  */
+//angular.module('clientApp', [
+//    'ngRoute', 'ngSanitize', 'restangular', 'ngTable', 'algoliasearch', 'algolia.autocomplete', 'ngMap', 'ui.bootstrap'
+//  ]).config(function ($routeProvider, RestangularProvider) {
+//    $routeProvider.when('/', {
+//        templateUrl: 'views/main.html'
+//        , controller: 'MainCtrl'
+//    }).when('/about', {
+//        templateUrl: 'views/about.html'
+//        , controller: 'AboutCtrl'
+//    }).when('/university', {
+//        templateUrl: 'views/university.html'
+//        , controller: 'UniversityCtrl'
+//    }).when('/ranking', {
+//        templateUrl: 'views/ranking.html'
+//        , controller: 'RankingCtrl'
+//    }).otherwise({
+//        redirectTo: '/'
+//    });
+//});
 angular.module('clientApp', [
     'ngRoute', 'ngSanitize', 'restangular', 'ngTable', 'algoliasearch', 'algolia.autocomplete', 'ngMap', 'ui.bootstrap'
   ]).config(function ($routeProvider, RestangularProvider) {
     $routeProvider.when('/', {
-        templateUrl: 'views/main.html'
-        , controller: 'MainCtrl'
+        templateUrl: 'views/university.html'
+        , controller: 'UniversityCtrl'
     }).when('/about', {
         templateUrl: 'views/about.html'
         , controller: 'AboutCtrl'
-    }).when('/university', {
-        templateUrl: 'views/university.html'
-        , controller: 'UniversityCtrl'
-    }).when('/ranking', {
-        templateUrl: 'views/ranking.html'
-        , controller: 'RankingCtrl'
     }).otherwise({
         redirectTo: '/'
     });
